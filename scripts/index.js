5c613928e25614ee33ee5935a3e370814b8482b1
AlekseyDream <u550@list.ru>
2023-01-04 20:57:45 +0300

    Исправленное

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let popup = document.querySelector ('.popup');
-let profileButtonEdit = document.querySelector ('.profile__buttonEdit');
-let popupButtonClose = popup.querySelector ('.popup__buttonClose');
+let profileButtonEdit = document.querySelector ('.profile__button-edit');
+let popupButtonClose = popup.querySelector ('.popup__button-close');
 let profileName = document.querySelector ('.profile__name');
 let profileJob = document.querySelector ('.profile__description');
 let input = document.querySelector ('.popup__form');
-let nameInput = input.querySelector ('.popup__formInput_data_name');
-let jobInput = input.querySelector ('.popup__formInput_data_about');
-let inputSumbitButton = input.querySelector ('.popup__buttonSave');
+let nameInput = input.querySelector ('.popup__form-input_data_name');
+let jobInput = input.querySelector ('.popup__form-input_data_about');
+let inputSumbitButton = input.querySelector ('.popup__button-save');
 
 function openPopup() {
     popup.classList.add('popup_opened');
