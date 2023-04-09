4356199a6c5a42f7886f1cb6806d086af72b71a0
AlekseyDream <u550@list.ru>
2023-04-09 14:21:24 +0300

    gg

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,6 +73,8 @@ const popupAvatar = new PopupWithForm(".popup_data_avatar", (userData) => {
 });
 popupAvatar.setEventListeners();
 
+console.log(popupAvatar);
+
 const popupWithImage = new PopupWithImage(".popup_data_image-add");
 popupWithImage.setEventListeners();
 
